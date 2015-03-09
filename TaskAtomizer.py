43a6144c601b50de35992a85464a527b95005bb2
Anthony Bashi <bashia@uvic.ca>
2015-03-09 14:10:45 -0700

    Added User class

--- TaskAtomizer.py
+++ TaskAtomizer.py
@@ -5,12 +5,20 @@ class Atom:
     def Atom():
         self.description = ""
 
+
 class Task:
     def Task():
         self.description = ""
         self.atoms = Atom():
+        self.priority = 0
 
 class Goal:
     def Goal():
         self.description = ""
         self.tasks = Task()
+        self.related = []
+
+class User:
+    def User():
+        self.name = ""
+        self.goals = []
